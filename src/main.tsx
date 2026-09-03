68d8133bbea1b2fa42cf1aff48e12e56a4f90440
atharvatamboli9171 <atharvatamboli9171@gmail.com>
2026-09-03 22:15:05 +0530

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,1 +1,16 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
 
+function App() {
+  return (
+    <div style={{ padding: "20px", fontSize: "24px" }}>
+      🚀 ScrapConnect is Working!
+    </div>
+  );
+}
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
