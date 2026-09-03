96d546595779a0b93129ce0a3f3bf39add7e7162
atharvatamboli9171 <atharvatamboli9171@gmail.com>
2026-09-03 22:17:16 +0530

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -3,9 +3,9 @@ import ReactDOM from "react-dom/client";
 
 function App() {
   return (
-    <div style={{ padding: "20px", fontSize: "24px" }}>
-      🚀 ScrapConnect is Working!
-    </div>
+    <h1 style={{ textAlign: "center", marginTop: "50px" }}>
+      🚀 ScrapConnect App Started!
+    </h1>
   );
 }
 
